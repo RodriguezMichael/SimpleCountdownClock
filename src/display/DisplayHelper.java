a15f7bf866fa7e0f4e66074e9262532f0023ea2b
RodriguezMichael <mrodriguez@ikanow.com>
2015-11-26 20:33:21 -0500

    push from ide

---
+++ src/display/DisplayHelper.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package display;
+
+/**
+ * @author Mino
+ *
+ */
+public class DisplayHelper {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello, World!2");
+	}
+
+}
